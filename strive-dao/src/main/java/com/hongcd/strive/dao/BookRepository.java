0297364a8b7d2fd2ebc14d461feb28464346d53b
hongcd <hongcandong@gmail.com>
2018-05-14 01:07:32 +0800

    修改maven install失败问题，移除entity模块

--- strive-dao/src/main/java/com/hongcd/strive/dao/BookRepository.java
+++ strive-dao/src/main/java/com/hongcd/strive/dao/BookRepository.java
@@ -1,7 +1,8 @@
 package com.hongcd.strive.dao;
 
 import com.hongcd.strive.entity.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends org.springframework.data.jpa.repository.JpaRepository<Book, String> {
+public interface BookRepository extends JpaRepository<Book, String> {
 
 }
